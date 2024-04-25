c27912fc76089ca067f2c0218d6a585953c37ee1
kirill201 <kirill.301522@gmail.com>
2024-04-25 10:30:38 +1000

    console_logger

---
+++ src/out/console-logger/console.logger.ts
@@ -0,0 +1,30 @@
+import {IStreamLogger} from "../../core/handlers/stream-logger.interface";
+
+export class ConsoleLogger implements IStreamLogger {
+
+    static instance: ConsoleLogger;
+
+    private constructor() {
+    }
+
+    public static getInstance(): ConsoleLogger {
+        if (!this.instance) {
+            this.instance = new ConsoleLogger()
+        }
+        return this.instance
+    }
+
+    log(...args: any[]): void {
+        console.log(...args)
+    }
+
+    error(...args: any[]): void {
+        console.log(...args)
+    }
+
+    end(): void {
+        console.log('Готово')
+    }
+
+
+}
